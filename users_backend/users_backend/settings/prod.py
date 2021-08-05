5e77aca671a84bc9aed8f694a91359ad88dd54f0
José Antonio González Morente <jgonzalezmorente@gmail.com>
2021-08-05 23:27:02 +0200

    Configuración del proyecto

---
+++ users_backend/users_backend/settings/prod.py
@@ -0,0 +1,25 @@
+from .base import * 
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'zenerdb',
+        'USER': 'zeneruser',
+        'PASSWORD': 'zener2021',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_ROOT = BASE_DIR.child( 'staticfiles' )
